659366f0765b847957af320cf98489cf3097a4c7
klichardova <katarina.lichardova@gmail.com>
2025-02-10 13:06:58 +0100

    Button remapping + getter functions

--- include/KMR_ps5.hpp
+++ include/KMR_ps5.hpp
@@ -35,11 +35,11 @@ namespace KMR::gamepads
  *              given that they can only take 3 possible values {-1, 0, 1}
  */
 enum ps5Buttons {
-    e_BTN_SELECT, e_BTN_START, e_BTN_MODE, 
-    e_BTN_NORTH, e_BTN_WEST, e_BTN_SOUTH, e_BTN_EAST,
-    e_BTN_TR, e_BTN_TR2, e_BTN_TL, e_BTN_TL2,
-    e_BTN_THUMBR, e_BTN_THUMBL,
-    e_ABS_HAT0X, e_ABS_HAT0Y, 
+    SELECT_BTN, START_BTN, PS_BTN, 
+    TRIANGLE_BTN, SQUARE_BTN, CROSS_BTN, CIRCLE_BTN,
+    R1_BTN, R2_BTN, L1_BTN, L2_BTN,
+    R3_BTN, L3_BTN,
+    UP_BTN, DOWN_BTN, LEFT_BTN, RIGHT_BTN,
     NBR_BUTTONS, UNDEF_BUTTONS
 };
 
@@ -47,24 +47,33 @@ enum ps5Buttons {
  * @brief   Enumerate of all axes present in a PS5
  */
 enum ps5Axes{
-    e_ABS_RX, e_ABS_RY, e_ABS_X, e_ABS_Y,
-    e_ABS_Z, e_ABS_RZ,
+    RSTICK_X, RSTICK_Y,
+    LSTICK_X, LSTICK_Y,
+    L2_AXIS, R2_AXIS,
     NBR_AXES, UNDEF_AXES
 };
 
+/**
+ * @brief   Class handling the PS5 monitoring and querying
+ */
 class PS5 {
 public:
     PS5();
     PS5(const char* gamepad_portname);
     ~PS5();
 
-    std::vector<int> m_buttons;
-    std::vector<float> m_axes;
-    std::mutex m_mutex;
+    int getButton(ps5Buttons button);
+    float getAxis(ps5Axes axis);
+    std::vector<int> getBulkButtons(std::vector<ps5Buttons> buttons);
+    std::vector<float> getBulkAxes(std::vector<ps5Axes> axes);
 
 private:
     bool m_stopThread;
     std::thread m_ps5_thread;
+    std::mutex m_mutex;
+
+    std::vector<int> m_buttons;
+    std::vector<float> m_axes;
 
     void updateGamepad(input_event ev);
     void gamepadLoop(const char* gamepad_portname);
--- source/KMR_ps5.cpp
+++ source/KMR_ps5.cpp
@@ -48,15 +48,12 @@ PS5::PS5()
     m_axes = vector<float>(NBR_AXES, 0);
 
     m_ps5_thread = thread(&KMR::gamepads::PS5::gamepadLoop, this, "/dev/input/event-ps5-gamepad");
-
-    cout << "Success: PS5 object created!" << endl;
 }
 
 
 /**
  * @brief       Create and initialize a PS5 object with a different gamepad than the default
  * @param[in]   gamepad_portname Port handling the gamepad, in the form of "/dev/input/eventX"
- * @retval      void
  */
 PS5::PS5(const char* gamepad_portname)
 {
@@ -67,8 +64,6 @@ PS5::PS5(const char* gamepad_portname)
     m_axes = vector<float>(NBR_AXES, 0);
 
     m_ps5_thread = thread(&KMR::gamepads::PS5::gamepadLoop, this, gamepad_portname);
-
-    cout << "Success: PS5 object created!" << endl;
 }
 
 
@@ -90,7 +85,6 @@ PS5::~PS5()
 /**
  * @brief       Read and save the current gamepad values. To loop in a thread
  * @param[in]   gamepad_portname Port handling the gamepad, in the form of /dev/input/eventX
- * @retval      void
  */
 void PS5::gamepadLoop(const char* gamepad_portname)
 {
@@ -111,9 +105,11 @@ void PS5::gamepadLoop(const char* gamepad_portname)
         libevdev_get_id_product(dev));
 
     // Start of loop
+    cout << "Gamepad monitoring starting" << endl;
     while(!m_stopThread) {
         struct input_event ev;
         rc = libevdev_next_event(dev, read_flag, &ev);
+
         if (rc == LIBEVDEV_READ_STATUS_SUCCESS)
             updateGamepad(ev);
         else if (rc == -EAGAIN)
@@ -132,7 +128,6 @@ void PS5::gamepadLoop(const char* gamepad_portname)
 /**
  * @brief       Save the new gamepad value into the correct table
  * @param[in]   ev Event indicating a change of gamepad value
- * @retval      void
  * @note        The values of the axes get remapped from [0,255] to [-1,1], 
  *              and the Y-axes inverted so that they point towards the front
  */
@@ -150,32 +145,132 @@ void PS5::updateGamepad(input_event ev)
 
     // A button's state has changed
     if (strcmp(event_type, "EV_KEY") == 0){
-        if      (strcmp(event_code, "BTN_SELECT") == 0) m_buttons[e_BTN_SELECT] = value;
-        else if (strcmp(event_code, "BTN_START") == 0)  m_buttons[e_BTN_START] = value;
-        else if (strcmp(event_code, "BTN_MODE") == 0)   m_buttons[e_BTN_MODE] = value;
-        else if (strcmp(event_code, "BTN_NORTH") == 0)  m_buttons[e_BTN_NORTH] = value;
-        else if (strcmp(event_code, "BTN_WEST") == 0)   m_buttons[e_BTN_WEST] = value;
-        else if (strcmp(event_code, "BTN_SOUTH") == 0)  m_buttons[e_BTN_SOUTH] = value;
-        else if (strcmp(event_code, "BTN_EAST") == 0)   m_buttons[e_BTN_EAST] = value;
-        else if (strcmp(event_code, "BTN_TR") == 0)     m_buttons[e_BTN_TR] = value;
-        else if (strcmp(event_code, "BTN_TR2") == 0)    m_buttons[e_BTN_TR2] = value;
-        else if (strcmp(event_code, "BTN_TL") == 0)     m_buttons[e_BTN_TL] = value;
-        else if (strcmp(event_code, "BTN_TL2") == 0)    m_buttons[e_BTN_TL2] = value;
-        else if (strcmp(event_code, "BTN_THUMBR") == 0) m_buttons[e_BTN_THUMBR] = value;
-        else if (strcmp(event_code, "BTN_THUMBL") == 0) m_buttons[e_BTN_THUMBL] = value;
+        if      (strcmp(event_code, "BTN_SELECT") == 0) m_buttons[SELECT_BTN] = value;
+        else if (strcmp(event_code, "BTN_START") == 0)  m_buttons[START_BTN] = value;
+        else if (strcmp(event_code, "BTN_MODE") == 0)   m_buttons[PS_BTN] = value;
+        else if (strcmp(event_code, "BTN_NORTH") == 0)  m_buttons[TRIANGLE_BTN] = value;
+        else if (strcmp(event_code, "BTN_WEST") == 0)   m_buttons[SQUARE_BTN] = value;
+        else if (strcmp(event_code, "BTN_SOUTH") == 0)  m_buttons[CROSS_BTN] = value;
+        else if (strcmp(event_code, "BTN_EAST") == 0)   m_buttons[CIRCLE_BTN] = value;
+        else if (strcmp(event_code, "BTN_TR") == 0)     m_buttons[R1_BTN] = value;
+        else if (strcmp(event_code, "BTN_TR2") == 0)    m_buttons[R2_BTN] = value;
+        else if (strcmp(event_code, "BTN_TL") == 0)     m_buttons[L1_BTN] = value;
+        else if (strcmp(event_code, "BTN_TL2") == 0)    m_buttons[L2_BTN] = value;
+        else if (strcmp(event_code, "BTN_THUMBR") == 0) m_buttons[R3_BTN] = value;
+        else if (strcmp(event_code, "BTN_THUMBL") == 0) m_buttons[L3_BTN] = value;
     }
 
     // The value of an absolute axis has changed
     else if (strcmp(event_type, "EV_ABS") == 0){
-        if     (strcmp(event_code, "ABS_RX") == 0)    m_axes[e_ABS_RX] =  rescaled_value;
-        else if(strcmp(event_code, "ABS_RY") == 0)    m_axes[e_ABS_RY] = -rescaled_value;
-        else if(strcmp(event_code, "ABS_X") == 0)     m_axes[e_ABS_X] =   rescaled_value;
-        else if(strcmp(event_code, "ABS_Y") == 0)     m_axes[e_ABS_Y] =  -rescaled_value;
-        else if(strcmp(event_code, "ABS_Z") == 0)     m_axes[e_ABS_Z] =   rescaled_value;
-        else if(strcmp(event_code, "ABS_RZ") == 0)    m_axes[e_ABS_RZ] =  rescaled_value;
-        else if(strcmp(event_code, "ABS_HAT0X") == 0) m_buttons[e_ABS_HAT0X] = value;
-        else if(strcmp(event_code, "ABS_HAT0Y") == 0) m_buttons[e_ABS_HAT0Y] = -value;
+        if     (strcmp(event_code, "ABS_RX") == 0)    m_axes[RSTICK_X] =  rescaled_value;
+        else if(strcmp(event_code, "ABS_RY") == 0)    m_axes[RSTICK_Y] = -rescaled_value;
+        else if(strcmp(event_code, "ABS_X") == 0)     m_axes[LSTICK_X] =  rescaled_value;
+        else if(strcmp(event_code, "ABS_Y") == 0)     m_axes[LSTICK_Y] = -rescaled_value;
+        else if(strcmp(event_code, "ABS_Z") == 0)     m_axes[L2_AXIS] =   rescaled_value;
+        else if(strcmp(event_code, "ABS_RZ") == 0)    m_axes[R2_AXIS] =   rescaled_value;
+        else if(strcmp(event_code, "ABS_HAT0X") == 0) {
+            switch (value)
+            {
+            case 0:
+                m_buttons[LEFT_BTN] = 0;
+                m_buttons[RIGHT_BTN] = 0;
+                break;
+            case 1:
+                m_buttons[LEFT_BTN] = 0;
+                m_buttons[RIGHT_BTN] = 1;
+                break;        
+            case -1:
+                m_buttons[LEFT_BTN] = 1;
+                m_buttons[RIGHT_BTN] = 0;
+                break;       
+            default:
+                break;
+            }
+        }
+        else if(strcmp(event_code, "ABS_HAT0Y") == 0) {
+            switch (value)
+            {
+            case 0:
+                m_buttons[UP_BTN] = 0;
+                m_buttons[DOWN_BTN] = 0;
+                break;
+            case 1:
+                m_buttons[UP_BTN] = 0;
+                m_buttons[DOWN_BTN] = 1;
+                break;        
+            case -1:
+                m_buttons[UP_BTN] = 1;
+                m_buttons[DOWN_BTN] = 0;
+                break;       
+            default:
+                break;
+            }
+        }       
     }
+}
+
+/**
+ * @brief       Get the inputs for the query buttons
+ * @param[in]   buttons Query buttons 
+ * @return      Inputs of the query buttons
+ */
+vector<int> PS5::getBulkButtons(vector<ps5Buttons> buttons)
+{
+    int nbrButtons = buttons.size();
+    vector<int> inputs(nbrButtons, 0);
+
+    // Grab the gamepad values from the internal storage
+    {
+        // Lock the mutex
+        scoped_lock lock(m_mutex);    
+        for (int i=0; i<nbrButtons; i++)
+            inputs[i] = m_buttons[buttons[i]];
+    }
+
+    return inputs;
+}
+
+/**
+ * @brief       Get the input for the query button
+ * @param[in]   button Query button
+ * @return      Input of the query button
+ */
+int PS5::getButton(ps5Buttons button)
+{
+    scoped_lock lock(m_mutex);    
+    return(m_buttons[button]);
+}
+
+/**
+ * @brief       Get the inputs for the query axes
+ * @param[in]   axes Query axes
+ * @return      Inputs of the query axes
+ */
+vector<float> PS5::getBulkAxes(std::vector<ps5Axes> axes)
+{
+    int nbrAxes = axes.size();
+    vector<float> inputs(nbrAxes, 0);
+
+    // Grab the gamepad values from the internal storage
+    {
+        // Lock the mutex
+        scoped_lock lock(m_mutex);    
+        for (int i=0; i<nbrAxes; i++)
+            inputs[i] = m_axes[axes[i]];
+    }
+
+    return inputs;
+}
+
+/**
+ * @brief       Get the input for the query axis
+ * @param[in]   axis Query axis
+ * @return      Input of the query axis
+ */
+float PS5::getAxis(ps5Axes axis)
+{
+    scoped_lock lock(m_mutex); 
+    return(m_axes[axis]);
 }
 
 
